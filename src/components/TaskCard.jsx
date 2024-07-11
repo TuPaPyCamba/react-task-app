2bc8538ef5a711f328c0d6a13b06be2a41f67ea2
Camba <fercambafcm@gmail.com>
2024-07-10 21:07:46 -0600

    red buttons

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -9,7 +9,7 @@ function TaskCard({ task }) {
         <div className="bg-gray-800 text-white p-4 rounded-md">
             <h1 className="text-xl font-bold capitalize">{task.titulo}</h1>
             <p className="text-gray-300 text-sm">{task.descripcion}</p>
-            <button className="bg-green-400 px-2 py-1 rounded-md mt-4 hover:bg-red-500" onClick={() => deleteTask(task.id)}>
+            <button className="bg-red-600 px-2 py-1 rounded-md mt-4 hover:bg-red-500" onClick={() => deleteTask(task.id)}>
                 Eliminar Tarea
             </button>
         </div>
